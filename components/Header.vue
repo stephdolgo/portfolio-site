689ba50e86ed2afd75f325048ad259f8103f19c9
stephdolgo <stephdolgo@gmail.com>
2022-06-22 00:37:13 -0400

    font size and margins adjusted

--- components/Header.vue
+++ components/Header.vue
@@ -1,28 +1,29 @@
 <template>
-    <header id="home"
+    <header id="home" 
         @mouseenter="$refs.customCursor.onMouseEnter()" 
         @mouseleave="$refs.customCursor.onMouseLeave()"
         @mousemove="e => $refs.customCursor.onMouseMove(e)"
-        class="hero grid-container">
+        class="hero grid-container"
+    >
         <HeroImg />
         <div class="grid-inner">
             <div class="hero-content">
                 <h1>Hi, I'm <span class="hero-content--highlighter"> Stephania</span>.</h1>
-                <p>Graphic Designer and Aspiring Web Developer working remotely.</p>
-                <Button 
-                        v-for="Button in Buttons" 
-                        :key="Button.key"
-                        :name="Button.name"
-                        :link="Button.link"
-                        :btnStyle="Button.btnStyle"
-                />
+                <p>Graphic Designer and self-taught web developer working remotely.</p>
+                <a href="./resume.pdf">
+                    <Button btnStyle="dark-btn">Resume</Button>
+                </a>
+                <a href="mailto:stephdolgo@gmail.com?subject=Inquiry:">
+                    <Button btnStyle="primary-btn">Contact</Button>
+                </a>
             </div>
         </div>
         <div class="grid-col--end">
             <div class="icon--scroll-down">
                 <a :href="anchorLink" 
-                @mouseover="$refs.customCursor.growMouse()" 
-                @mouseout="$refs.customCursor.shrinkMouse()">
+                    @mouseover="$refs.customCursor.growMouse()" 
+                    @mouseout="$refs.customCursor.shrinkMouse()"
+                >
                 <img src="/icons/arrow.svg" alt="scroll down arrow">
                 </a>
             </div>
@@ -31,29 +32,12 @@
     </header>
 </template>
 
-<script>
-    import Button from './Button'
-    import HeroImg from './HeroImg' 
-    import CustomCursor from './CustomCursor'    
-
+<script> 
     export default {
         name: 'Header',
         props: [
             "anchorLink"
-        ],
-        components: {
-            Button,
-            HeroImg,
-            CustomCursor
-        },
-        data() {
-            return {
-                Buttons: [
-                    {key: 1, name:'Resume', link:'./resume.pdf', btnStyle:'dark-btn'},
-                    {key: 2, name:'Contact', link:'mailto:stephdolgo@gmail.com?subject=Inquiry:', btnStyle:'pink-btn'},
-                ],
-            }
-        },
+        ]
     }
 </script>
 
@@ -67,12 +51,23 @@
 
         .hero-content {
             position: absolute;
-            top: 50%;
+            top: 40%;
             color: var(--white);
             z-index: 5;
+            margin-left: 20px;
 
             .hero-content--highlighter {
-                background: linear-gradient(to top, var(--hot-pink) 10%, transparent 0%);
+                background: linear-gradient(to top, var(--accent-tert) 10%, transparent 0%);
+            }
+
+            h1 {
+                font-size: 4em;
+                margin-bottom: 0;
+            }
+
+            p {
+                font-size: 1.35em;
+                color: var(--accent-primary);
             }
 
         }
