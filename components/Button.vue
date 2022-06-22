eb1531d64ac13455db2f0687f772512cd41e94d9
stephdolgo <stephdolgo@gmail.com>
2022-06-22 00:34:02 -0400

    changed to use slots for easier use

--- components/Button.vue
+++ components/Button.vue
@@ -1,10 +1,17 @@
 <template>
-    <a :href="link">
-        <button type="button" :class="btnStyle">
-            {{name}}
-        </button>
-    </a>
+    <button type="button" :class="btnStyle">
+        <slot /> 
+    </button>
 </template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: [
+            "btnStyle"
+        ]
+    }
+</script>
 
 <style lang="scss">
     button {
@@ -16,15 +23,36 @@
         margin: 4px 2px;
         cursor: pointer;
     }
-</style>
-
-<script>
-    export default {
-        name: 'Button',
-        props: [
-                "name",
-                "link",
-                "btnStyle"
-        ]
+    
+    .primary-btn {
+        color: var(--dark);
+        background-color: var(--accent-primary);
+        border: none;
+        border-radius: 15px;
+        transition: 0.5s ease;
+        &:hover,
+        &:focus {
+            box-shadow: inset 15em 0 0 0 var(--accent-secondary);
+        }
     }
-</script>
+
+    .dark-btn {
+        color: var(--light);
+        background-color: var(--dark);
+        border: 2px soild var(--light);
+        border-radius: 15px;
+        transition: 0.5s ease;
+        &:hover,
+        &:focus {
+            box-shadow: inset 15em 0 0 0 var(--accent-secondary);
+            color: var(--dark);
+        }
+    }
+
+    .light-btn {
+        color: var(--dark);
+        background-color: var(--light);
+        border: 2px soild var(--dark);
+        border-radius: 15px;
+    }
+</style>
